95d275d738f91808a5c2d30a424636fdeceacd74
IZiadI <ziadashraf727@gmail.com>
2026-05-26 05:49:02 +0300

    cards wrapping

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"4c525dac5ebe5971c5708ef73558ed8edcf4a3
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2115002774" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "197828609" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
